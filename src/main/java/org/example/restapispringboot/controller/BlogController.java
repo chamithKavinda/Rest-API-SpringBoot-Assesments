a002b956fc49421340b34d3313bc6d6689f52e9d
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-31 21:58:47 +0530

    response passed the frontend

--- src/main/java/org/example/restapispringboot/controller/BlogController.java
+++ src/main/java/org/example/restapispringboot/controller/BlogController.java
@@ -1,6 +1,7 @@
 package org.example.restapispringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,5 +15,10 @@ public class BlogController {
     @PostMapping("number")
     public int getNumber(){
         return 1;
+    }
+
+    @GetMapping("/number/{number}")
+    public String getNumber(@PathVariable String number) {
+        return number;
     }
 }
