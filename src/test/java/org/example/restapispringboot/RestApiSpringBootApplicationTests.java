a9d3a33632b72ef91c90435681612f6f789499e4
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-31 20:55:10 +0530

    initial project

---
+++ src/test/java/org/example/restapispringboot/RestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
