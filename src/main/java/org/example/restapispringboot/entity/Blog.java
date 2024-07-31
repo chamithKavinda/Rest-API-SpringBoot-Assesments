677bc524f5399c5d4ba3df39250d21f85b0efcdc
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-31 22:04:42 +0530

    Blog entity implement

---
+++ src/main/java/org/example/restapispringboot/entity/Blog.java
@@ -0,0 +1,16 @@
+package org.example.restapispringboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Blog {
+    @Id
+    private int id;
+    private String title;
+    private String content;
+}
