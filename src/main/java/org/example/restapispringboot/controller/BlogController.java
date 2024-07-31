d5c5b1283d4f135b33a618cec84f0f1cfe56458a
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-31 21:54:46 +0530

    request get mapping implement

---
+++ src/main/java/org/example/restapispringboot/controller/BlogController.java
@@ -0,0 +1,12 @@
+package org.example.restapispringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/blog")
+public class BlogController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
