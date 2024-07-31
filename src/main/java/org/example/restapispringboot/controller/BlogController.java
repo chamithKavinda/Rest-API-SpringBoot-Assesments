79584ee8c663e710805e293dd53e4cf85f11fa85
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-31 21:56:15 +0530

    number end point implement

--- src/main/java/org/example/restapispringboot/controller/BlogController.java
+++ src/main/java/org/example/restapispringboot/controller/BlogController.java
@@ -1,6 +1,7 @@
 package org.example.restapispringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/blog")
@@ -8,5 +9,10 @@ public class BlogController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("number")
+    public int getNumber(){
+        return 1;
     }
 }
