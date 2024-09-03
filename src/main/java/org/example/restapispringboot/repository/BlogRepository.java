26ee47fae604f7531bad2e830dec615a4b12bd1c
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-03 11:56:40 +0530

    content update

--- src/main/java/org/example/restapispringboot/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.restapispringboot.repository;
-
-import org.example.restapispringboot.entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog,Integer> {
-}
