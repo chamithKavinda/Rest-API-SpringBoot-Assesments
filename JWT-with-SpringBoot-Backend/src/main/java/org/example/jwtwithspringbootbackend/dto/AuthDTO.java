1815f471a63f8a59d231df45108841fe4a179915
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-03 18:06:55 +0530

    AuthDTO implemented

---
+++ JWT-with-SpringBoot-Backend/src/main/java/org/example/jwtwithspringbootbackend/dto/AuthDTO.java
@@ -0,0 +1,16 @@
+package org.example.jwtwithspringbootbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Component
+public class AuthDTO {
+    private String email;
+    private String token;
+    private String refreshToken;
+}
