b6d94bc06aa99e0ddcfaf2922c67c6ab8e4c0ba3
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-03 18:59:39 +0530

    generate token , validate token implemented

--- JWT-with-SpringBoot-Backend/src/main/java/org/example/jwtwithspringbootbackend/JwtWithSpringBootBackendApplication.java
+++ JWT-with-SpringBoot-Backend/src/main/java/org/example/jwtwithspringbootbackend/JwtWithSpringBootBackendApplication.java
@@ -1,7 +1,9 @@
 package org.example.jwtwithspringbootbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -10,5 +12,8 @@ public class JwtWithSpringBootBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(JwtWithSpringBootBackendApplication.class, args);
     }
-
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
