3ba2a344cac6bca0d9e1f4da44b08317bf5946c6
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-03 17:06:47 +0530

    blog controller implemented

--- JWT-with-SpringBoot-Backend/src/main/java/org/example/jwtwithspringbootbackend/JwtWithSpringBootBackendApplication.java
+++ JWT-with-SpringBoot-Backend/src/main/java/org/example/jwtwithspringbootbackend/JwtWithSpringBootBackendApplication.java
@@ -2,10 +2,11 @@ package org.example.jwtwithspringbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("api/v1/auth")
 public class JwtWithSpringBootBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JwtWithSpringBootBackendApplication.class, args);
     }
