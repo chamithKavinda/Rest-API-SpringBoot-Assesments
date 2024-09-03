7407d07b8a2357cbf1cdad12e4a481850816d848
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-03 12:23:38 +0530

    project initialized

---
+++ JWT-with-SpringBoot-Backend/src/main/java/org/example/jwtwithspringbootbackend/JwtWithSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtwithspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithSpringBootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtWithSpringBootBackendApplication.class, args);
+    }
+
+}
---
+++ JWT-with-SpringBoot-Backend/src/test/java/org/example/jwtwithspringbootbackend/JwtWithSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtwithspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithSpringBootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
